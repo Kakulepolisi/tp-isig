348b9fa9b4957849b479c14755330b7eb9c0c419
Kakulepolisi <kakulejonas8@gmail.com>
2024-11-20 10:37:51 +0100

    Add files via upload

---
+++ exemple.js
@@ -0,0 +1,15 @@
+//a=5;b=6;c=a+b;
+//alert(c);
+//alert("robert"+ " " +"kalere")
+//let carname="bmw";
+//alert("le nom de la voiture est" + " " + carname);
+//let x = "16" + "volvo";
+//alert(x);
+//let value=tocelcious(77);
+//function tocelcious(farheineit) {
+    
+//    alert((5/9)*(farheineit-32));
+//}
+let text = "robert";
+let car = text.charAt(1);
+alert(car);
